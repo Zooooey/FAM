b500d73ee6109f9fc6cc1695c60686b4cef4779f
Zoey <cuiziki@163.com>
2022-10-16 17:40:49 +0800

    .

--- src/bfs.hpp
+++ src/bfs.hpp
@@ -145,11 +145,11 @@ public:
                       uint32_t const, uint32_t *const edges, uint32_t const n) noexcept {
       for (uint32_t i = 0; i < n; ++i) {// push out updates //make parallel
         uint32_t w = edges[i];
-        clock_gettime(CLOCK_MONOTONIC, &t1);
+        clock_gettime(CLOCK_MONOTONIC, &atomic_t1);
         if (vtable[w].update_atomic(round)) {
           next_frontier->set_bit(w);// activate w
         }
-        clock_gettime(CLOCK_MONOTONIC, &t2);
+        clock_gettime(CLOCK_MONOTONIC, &atomic_t2);
         famgraph::timespec_diff(&atomic_t2, &atomic_t1, &atomic_res);
         ctx->stats.atomic_time.local() += atomic_res.tv_sec * 1000000000L + atomic_res.tv_nsec;
       }
