d987c8bbf83d86f4d5e7481d264b9f977d195803
ccy <cuiziki@163.com>
2023-02-26 10:35:47 +0800

    .

--- include/fam_common.hpp
+++ include/fam_common.hpp
@@ -15,27 +15,27 @@ namespace fam_thp{
     inline int huge_page_select(void *addr, size_t length, FAM_THP_FLAG user_select, FAM_THP_FLAG current_select){
         int mad_ret;
         if(user_select & current_select){
-            mad_ret = madvice(addr, length, MADV_HUGEPAGE);
+            mad_ret = madvise(addr, length, MADV_HUGEPAGE);
         }else {
-            mad_ret = madvice(addr, length, MADV_NOHUGEPAGE);
+            mad_ret = madvise(addr, length, MADV_NOHUGEPAGE);
         }
         return mad_ret;
     }
 
     int advice_edge_thp(void *addr, size_t length, FAM_THP_FLAG fam_thp_flag){
         if(0 != huge_page_select(addr, length, fam_thp_flag, THP_EDGE_ARRAY)){
-            BOOST_LOG_TRIVIAL(fatal) << "madvice for EDGE array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
+            BOOST_LOG_TRIVIAL(fatal) << "madvise for EDGE array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
         }
     }
     int advice_vertex_thp(void *addr, size_t length, FAM_THP_FLAG fam_thp_flag){
         if(0 != huge_page_select(addr, length, fam_thp_flag, THP_VERTEX_ARRAY)){
-            BOOST_LOG_TRIVIAL(fatal) << "madvice for VERTEX array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
+            BOOST_LOG_TRIVIAL(fatal) << "madvise for VERTEX array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
         }
     }
 
     void advice_prop_thp(void *addr, size_t length, FAM_THP_FLAG fam_thp_flag){
         if(0 != huge_page_select(addr, length, fam_thp_flag, THP_PROPERTY_ARRAY)){
-            BOOST_LOG_TRIVIAL(fatal) << "madvice for PROPERTY array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
+            BOOST_LOG_TRIVIAL(fatal) << "madvise for PROPERTY array of FAM failed! " <<"strerror(errno):"<<strerror(errno);  
         }
     }
 }
