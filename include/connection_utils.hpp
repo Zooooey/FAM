ad710a0d23a2fb52d8affcb6b81d5a5b146d0aa1
cuiziki <cuiziki@163.com>
2023-05-04 21:32:29 +0800

    .

--- include/connection_utils.hpp
+++ include/connection_utils.hpp
@@ -9,8 +9,6 @@
 #include "AbstractServer.h"
 #include <rdma/rdma_cma.h>
 
-#define TEST_NZ(x) do { if ( (x)) rc_die("error: " #x " failed (returned non-zero)." ); } while (0)
-#define TEST_Z(x)  do { if (!(x)) rc_die("error: " #x " failed (returned zero/null)."); } while (0)
 
 typedef void (*pre_conn_cb_fn)(struct rdma_cm_id *id);
 typedef void (*connect_cb_fn)(struct rdma_cm_id *id);
@@ -20,7 +18,6 @@ typedef void (*disconnect_cb_fn)(struct rdma_cm_id *id);
 void rc_init(pre_conn_cb_fn, connect_cb_fn, completion_cb_fn, disconnect_cb_fn);
 void rc_client_loop(const char *host, const char *port, struct client_context* context);
 void rc_disconnect(struct rdma_cm_id *id);
-void rc_die(const char *message);
 struct ibv_pd * rc_get_pd();
 unsigned long rc_get_num_connections();
 
